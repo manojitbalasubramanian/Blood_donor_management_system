4416c6a2e22afd2d39861d2783071f669d332e4c
manojitbalasubramanian <manojitbalasubramanianr@gmail.com>
2025-07-19 12:57:45 +0530

    server changes

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,11 @@ const PORT = process.env.PORT;
 
 import cors from 'cors';
 app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your frontend URL
+    origin: 'http://localhost:0000', // Replace with your frontend URL
   }));
 
-// app.use(express.json());
-// app.use(cookieParser());
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/auth",authRoutes);
 
