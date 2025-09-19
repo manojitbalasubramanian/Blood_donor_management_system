9f111d86e04eea1bcdc147ff1d7e045613c07d99
manojitbalasubramanian <manojitbalasubramanianr@gmail.com>
2025-09-19 21:21:09 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ import cors from 'cors';
 
 // CORS configuration
 const corsOptions = {
-    origin: ["http://localhost:5678", "http://localhost:5173"],
+    origin: ["https://blood-donor-management-system-v4qz.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
